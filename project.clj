9d55a8709fc5382d1a812cd8d9ab2204e54d4392
Kyle Burton <kyle.burton@gmail.com>
2021-12-12 14:03:00 -0800

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject corpus-enormous "0.1.6-SNAPSHOT"
+(defproject corpus-enormous "0.1.6"
   :description "Randomized Data Set Generator"
   :url "https://github.com/kyleburton/corpus-enormous"
   :license {:name "Eclipse Public License"
